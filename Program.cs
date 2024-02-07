c414428136dc8aef4e357286de9713f9f99995f2
Aleksandr Khan <manchig1990@gmail.com>
2024-02-07 21:53:41 +1000

    all commit

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя).
-
 // Например:
 // 4 -> 16
 // -3 -> 9
 // -7 -> 49
+
 
 // System.Console.WriteLine("Введите целое число: ");
 // int num = Convert.ToInt32(Console.ReadLine());
@@ -48,33 +48,34 @@
 // 3 -> Среда
 // 5 -> Пятница
 
-System.Console.WriteLine("Введите число дня недели");
-int num = Convert.ToInt32(Console.ReadLine());
-int monday = 1;
-int tuesday = 2;
-int wednesday = 3;
-int thursday = 4;
-int friday = 5;
-int saturday = 6;
-int sunday = 7;
 
-if (num == monday)
-{
-    System.Console.WriteLine("Понедельник");
-}
-else if (num == tuesday)
-    System.Console.WriteLine("Вторник");
-if (num == wednesday)
-    System.Console.WriteLine("Среда");
-else if (num == thursday)
-    System.Console.WriteLine("Четверг");
-if (num == friday)
-    System.Console.WriteLine("Пятница");
-else if (num == saturday)
-    System.Console.WriteLine("Суббота");
-if (num == sunday)
-    System.Console.WriteLine("Воскресенье");
-if (num > 7)
-    System.Console.WriteLine("Вы ввели не правильно число");
-if (num < 1)
-    System.Console.WriteLine("Вы ввели не правильно число");
+// System.Console.WriteLine("Введите число дня недели");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int monday = 1;
+// int tuesday = 2;
+// int wednesday = 3;
+// int thursday = 4;
+// int friday = 5;
+// int saturday = 6;
+// int sunday = 7;
+
+// if (num == monday)
+// {
+//     System.Console.WriteLine("Понедельник");
+// }
+// else if (num == tuesday)
+//     System.Console.WriteLine("Вторник");
+// if (num == wednesday)
+//     System.Console.WriteLine("Среда");
+// else if (num == thursday)
+//     System.Console.WriteLine("Четверг");
+// if (num == friday)
+//     System.Console.WriteLine("Пятница");
+// else if (num == saturday)
+//     System.Console.WriteLine("Суббота");
+// if (num == sunday)
+//     System.Console.WriteLine("Воскресенье");
+// if (num > 7)
+//     System.Console.WriteLine("Вы ввели не правильно число");
+// if (num < 1)
+//     System.Console.WriteLine("Вы ввели не правильно число");
