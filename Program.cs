f4ad916c2e62534ab5891c30d478308d0ba9a30d
Aleksandr Khan <manchig1990@gmail.com>
2024-02-10 15:13:37 +1000

    complete

--- Program.cs
+++ Program.cs
@@ -135,19 +135,112 @@
 
 // System.Console.WriteLine("Введите число: ");
 // int num = Convert.ToInt32(Console.ReadLine());
-// int digit = num % 10;
-// System.Console.WriteLine("Остаток равен " + digit);
+// System.Console.WriteLine("Остаток равен " + num%10);
 
 //******************************************************
 
 // Задача. Выводить 3е число если оно есть, если нет то прописать, что его нет.
 
-System.Console.WriteLine("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-if (num >= 100)
-{
-while (num / 1000 > 0)
-    num /= 10; // num = num / 10
-    System.Console.WriteLine(num%10);
-}
-else System.Console.WriteLine("Третьей цифры нет");
+// System.Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num >= 100)
+// {
+// while (num / 1000 > 0)
+//     num /= 10; // num = num / 10
+//     System.Console.WriteLine(num%10);
+// }
+// else System.Console.WriteLine("Третьей цифры нет");
+
+//******************************************************ПРАКТИЧЕСКОЕ ЗАДАНИЕ*****************************
+
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// System.Console.WriteLine("Введите первое число: ");
+// int numA = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите второе число: ");
+// int numB = Convert.ToInt32(Console.ReadLine());
+// if (numA > numB)
+//     System.Console.Write("Максимальное число: " + numA);
+// else
+//     System.Console.Write("Максимальное число: " + numB);
+
+//******************************************************
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// System.Console.WriteLine("Введите первое число: ");
+// int numA = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите второе число: ");
+// int numB = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите третье число: ");
+// int numC = Convert.ToInt32(Console.ReadLine());
+// int max = numA;
+// if (max < numB)
+// {
+//     max = numB;
+// }
+// else if (max < numC)
+// {
+//     max = numC;
+// }
+// System.Console.WriteLine("Максимальное число: " + max);
+
+//******************************************************
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// System.Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// num %= 2;
+// if (num == 0)
+//     System.Console.WriteLine("Вы ввели чётное числов");
+// else    
+//     System.Console.WriteLine("Вы ввели не чётное числов");
+
+//******************************************************
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// System.Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int cheackNum = 2;
+// if (cheackNum > num)
+// System.Console.WriteLine("Нет чётных чисел");
+// while (cheackNum <= num)
+// {
+//     System.Console.Write(cheackNum + " ");
+//     cheackNum += 2;
+// }
+
+//******************************************************
+
+// Задача 7 HARD по желанию - идет за 2 обязательных Напишите программу, которая принимает на вход целое число любой разрядности и на выходе показывает третью цифру слева этого числа или говорит, что такой цифры нет. Через строку решать нельзя.
+// 456111 -> 6
+// 78 -> нет
+// 9146548 -> 4
+// 3 -> нет
+
+// System.Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num >= 100)
+// {
+// while (num / 1000 > 0)
+//     num /= 10; // num = num / 10
+//     System.Console.WriteLine(num%10);
+// }
+// else System.Console.WriteLine("Третьей цифры нет");
